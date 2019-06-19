de2506a8b05cfd647f4ac0ec6761aa2b17b2bda4
lwj <15075325849@163.com>
2019-06-19 11:46:38 +0800

    修改mapper错误类型

--- hcycom-track/src/main/java/com/hcycom/ctginms/web/rest/FmRest.java
+++ hcycom-track/src/main/java/com/hcycom/ctginms/web/rest/FmRest.java
@@ -15,6 +15,7 @@ package com.hcycom.ctginms.web.rest;
     import io.swagger.annotations.ApiImplicitParams;
     import io.swagger.annotations.ApiOperation;
     import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.beans.factory.annotation.Value;
     import org.springframework.http.HttpHeaders;
     import org.springframework.http.HttpStatus;
     import org.springframework.http.MediaType;
@@ -36,6 +37,8 @@ package com.hcycom.ctginms.web.rest;
 @RequestMapping(value="/api/File-upload-and-file-download")
 @Api(tags={"File-upload-and-file-download"},description="点位管理和其他文件管理中的文件上传与下载")
 public class FmRest {
+    @Value("${pointpath}")
+    String pointpath;
     @Autowired
     private FmService fileService;
     @Autowired
@@ -53,9 +56,9 @@ public class FmRest {
     })
     public ResponseEntity<Fm> InsertUpFile(MultipartFile uploadFile, HttpServletRequest request,String fmurl,String pid,String reportcode) throws IOException {
         //String fileUrl ="/opt/tomcat/apache-tomcat-8.5.23/webapps/file/otherFiles/"+fmurl;
-        String url=System.getProperty("user.dir").replaceAll("\\\\", "/");
-        System.out.println("------------url---------------"+url);
-        String fileUrl =url+File.separator+"other_files"+File.separator+fmurl;
+        //String url=System.getProperty("user.dir").replaceAll("\\\\", "/");
+        System.out.println("------------url---------------"+pointpath);
+        String fileUrl =pointpath+File.separator+"other_files"+File.separator+fmurl;
         System.out.println("____________originalFileName_____________" + fmurl);
         //换成  服务器地址+ fileUrl
 //        fileUrl = request.getSession().getServletContext().getRealPath(fileUrl);
@@ -72,147 +75,90 @@ public class FmRest {
         return new ResponseEntity<Fm>(fileInfo, HttpStatus.OK);
     }
 
+    @GetMapping("/downfile1")
+    @Timed
+    @ApiOperation(value ="aaa")
+    public String downloadFile(@RequestBody JSONObject dwtxt,
+                                    HttpServletRequest request,
+                                    HttpServletResponse response){
+        String filepath=dwtxt.getString("fnUrl");
+        String filename=dwtxt.getString("fnName");
+        File file = new File(filepath);
+        if (file.exists()) {
+            response.setContentType("application/force-download");// 设置强制下载不打开
+            response.addHeader("Content-Disposition",
+                "attachment;fnName=" + filename);// 设置文件名
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+
 
 
 
     @GetMapping("/downfile")
     @Timed
-    @ApiOperation(value = "其它文件管理中文件下载,(other_files表的操作，在其它文件中进行下载)")
-    public String downfile(String filename,String id,HttpServletRequest request,HttpServletResponse response) throws IOException {
-        System.out.println(filename+id);
-        //根据id获取文件上传路径
-        Fm upFile=fileService.selectFileById(Integer.parseInt(id));
-        String fileName = upFile.getFnUrl()+"/"+filename;
-        //防止中文乱码
-        filename = URLEncoder.encode(filename,"UTF-8");
-        System.out.println(filename);
-        //获取输入流
-        InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
-        //设置文件下载头
-        response.addHeader("Content-Disposition", "attachment;fileName=" + filename);
-        //1.设置文件ContentType类型，这样设置，会自动判断下载文件类型
-        response.setContentType("multipart/form-data");
-        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
-        int len = 0;
-        while((len = bis.read()) != -1){
-            out.write(len);
-            out.flush();
-        }
-        out.close();
-        return "OK";
-    }
-
-
-    @GetMapping("/downfile1")
-    @Timed
-    @ApiOperation(value ="aaa")
-    public static void downloadFile(HttpServletResponse response,String fileName,String path){
-        if (fileName != null) {
-            //设置文件路径
-            File file = new File(path);
-            if (file.exists()) {
-                response.setHeader("content-type", "application/octet-stream");
-                response.setContentType("application/octet-stream");
-                try {
-                    response.setHeader("Content-Disposition", "attachment;filename="+new String(fileName.getBytes("utf-8"),"ISO-8859-1"));
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                byte[] buffer = new byte[1024];
-                FileInputStream fis = null;
-                BufferedInputStream bis = null;
-                try {
-                    fis = new FileInputStream(file);
-                    bis = new BufferedInputStream(fis);
-                    OutputStream os = response.getOutputStream();
-                    int i = bis.read(buffer);
-                    while (i != -1) {
-                        os.write(buffer, 0, i);
-                        i = bis.read(buffer);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (fis != null) {
-                        try {
-                            fis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
-
-
-
-   /* @RequestMapping("/down")
-    public void down(HttpServletRequest request,HttpServletResponse response,Integer fileId) throws Exception{
-        FmService dao = new FmServiceImpl();
-
-        Fm fileInfo = dao.selectFileById(fileId);
-        String fileName1 = fileInfo.getFnName();
-        System.out.println("-------fileInfo--------"+fileInfo);
-        //模拟文件，myfile.txt为需要下载的文件
-        String fileName = request.getSession().getServletContext().getRealPath("upload")+"/"+fileName1;
-        //获取输入流
-        InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
-        //假如以中文名下载的话
-        String filename = fileName1;
-        //转码，免得文件名中文乱码
-        filename = URLEncoder.encode(filename,"UTF-8");
-        //设置文件下载头
-        response.addHeader("Content-Disposition", "attachment;filename=" + filename);
-        //1.设置文件ContentType类型，这样设置，会自动判断下载文件类型
-        response.setContentType("multipart/form-data");
-        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
-        int len = 0;
-        while((len = bis.read()) != -1){
-            out.write(len);
-            out.flush();
-        }
-        out.close();
-    }*/
-
-
-
-
-   /* @RequestMapping("/download")
-    public ResponseEntity<byte[]> download(Integer fileId){
-        FmService dao = new FmServiceImpl();
+    @ApiOperation(value ="aqqq")
+    public HttpServletResponse download(String path, HttpServletResponse response) {
         try {
-            Fm fileInfo = dao.selectFileById(fileId);
-            String fileUrl = fileInfo.getFnUrl();
-            String fileName = fileInfo.getFnName();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentDispositionFormData("attachment", fileName);
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            // path是指欲下载的文件的路径。
+            File file = new File(path);
+            // 取得文件名。
+            String filename = file.getName();
+            // 取得文件的后缀名。
+            String ext = filename.substring(filename.lastIndexOf(".") + 1).toUpperCase();
 
-
-            ResponseEntity<byte[]> entity =
-                new ResponseEntity<byte[]>(FileUtil.readFileToByteArray(new File(fileUrl)), headers,HttpStatus.CREATED);
-            return entity;
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            // 以流的形式下载文件。
+            InputStream fis = new BufferedInputStream(new FileInputStream(path));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.addHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes()));
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        return null;
-    }*/
-
-
+        return response;
+    }
 
 
 
